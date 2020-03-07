98e605d65ab5c869d5b00f69ecfe2ad23f9ea53f
mayanksh99 <mayankshakya992@gmail.com>
2020-03-07 23:43:16 +0530

    --update: first aid routes complete

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -43,3 +43,48 @@ module.exports.deletePrecautions = async (req, res) => {
 		});
 	}
 };
+
+module.exports.getAid = async (req, res) => {
+	let data = await FirstAid.find().sort({ createdAt: "desc" });
+	res.status(200).json({ message: "success", error: false, data });
+};
+
+module.exports.addAid = async (req, res) => {
+	let { woundType } = req.body;
+	let prevData = await FirstAid.find({ woundType });
+	if (prevData != "") {
+		res.status(200).json({ message: "Already added", error: false });
+	} else {
+		let newData = req.body;
+		let data = await FirstAid.create(newData);
+		res.status(200).json({ message: "success", error: false, data });
+	}
+};
+
+module.exports.updateAid = async (req, res) => {
+	let _id = req.params.id;
+	let { woundType, treatment } = req.body;
+	let data = await FirstAid.findOne({ _id });
+	if (data) {
+		data.woundType = woundType;
+		data.treatment = treatment;
+		await data.save();
+		data = await FirstAid.findOne({ _id });
+		res.status(200).json({ message: "success", error: false, data });
+	}
+};
+
+module.exports.deleteAid = async (req, res) => {
+	let _id = req.params.id;
+	let data = await FirstAid.findOne({ _id });
+	if (data) {
+		await data.delete();
+		res.status(200).json({ message: "success", error: false, data: null });
+	} else {
+		res.status(400).json({
+			message: "Invalid data",
+			error: false,
+			data: null
+		});
+	}
+};
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -6,7 +6,11 @@ const {
 	getPrecautions,
 	addPrecautions,
 	updatePrecautions,
-	deletePrecautions
+	deletePrecautions,
+	getAid,
+	addAid,
+	updateAid,
+	deleteAid
 } = require("../../../controllers/index_controller");
 
 // middlewares
@@ -18,6 +22,11 @@ router.get("/precautions", catchErrors(getPrecautions));
 router.post("/precautions", catchErrors(addPrecautions));
 router.put("/precautions/:id", catchErrors(updatePrecautions));
 router.delete("/precautions/:id", catchErrors(deletePrecautions));
+
+router.get("/aid", catchErrors(getAid));
+router.post("/aid", catchErrors(addAid));
+router.put("/aid/:id", catchErrors(updateAid));
+router.delete("/aid/:id", catchErrors(deleteAid));
 
 // export router
 module.exports = router;
